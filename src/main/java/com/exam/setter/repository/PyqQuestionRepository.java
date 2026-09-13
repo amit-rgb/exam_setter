ca1fb1f4066c740b33504a16a65ee759e455d938
amit-rgb <63780141+amit-rgb@users.noreply.github.com>
2026-09-13 18:00:48 +0530

    Add previous-year question repository

---
+++ src/main/java/com/exam/setter/repository/PyqQuestionRepository.java
@@ -0,0 +1,12 @@
+package com.exam.setter.repository;
+
+import com.exam.setter.entity.PyqQuestionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PyqQuestionRepository extends JpaRepository<PyqQuestionEntity, UUID> {
+    List<PyqQuestionEntity> findByExamIdIgnoreCaseOrderByYearAscQuestionNumberAsc(String examId);
+    List<PyqQuestionEntity> findByExamIdIgnoreCaseAndSubjectIgnoreCase(String examId, String subject);
+}
